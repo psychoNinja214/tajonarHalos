1b6e12f282367dacd2972dc187590bee5366d40f
psycho <psychoNinja214@gmail.com>
2025-11-10 18:34:03 -0600

    finally set up

--- lito/urls.py
+++ lito/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views  # make sure you’re importing from the same directory as this file
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),
+    path('', views.index, name='index'),
 ]
